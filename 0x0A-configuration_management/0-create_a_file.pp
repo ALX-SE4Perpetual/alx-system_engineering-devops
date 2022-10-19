0fe5dc5e028336a63202617156f2158e1130f602
root <root@DESKTOP-0PQA5EB.localdomain>
2022-10-19 22:38:37 +0100

    Create a new puppet file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,9 @@
 # Create a new text file with requirements
 file { '/tmp/school':
-  ensure => present,
+  ensure => file,
+  path => '/tmp/school',
   mode => '0744',
   owner => 'www-data',
   group => 'www-data',
-  content => 'I love Puppet',
+  content => 'I love Puppet'
 }
