43948dd8b88c997c2b92727fa7929ed29055ce69
root <root@DESKTOP-0PQA5EB.localdomain>
2022-10-19 23:38:29 +0100

    esecute a command

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,5 @@
 # Create a manifest that kills a process named killmenow.
 
-exec {'pkill killmenow':
-    command => '#!/usr/bin/pkill -f killmenow''
+exec { 'pkill killmenow':
+    command => '!/usr/bin/pkill -f killmenow''
 }
